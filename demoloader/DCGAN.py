1f4799d64dda8174d43950ff76448acbcfaf175c
ioo0s <iosmosis@163.com>
2023-05-18 15:57:05 +0800

    fix invalid size error for fmnist DCGAN

--- demoloader/DCGAN.py
+++ demoloader/DCGAN.py
@@ -1,12 +1,13 @@
 import torch.nn as nn
 
+
 class Generator(nn.Module):
     def __init__(self, ngpu=1, nc=3, nz=100, ngf=64):
         super(Generator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
@@ -14,15 +15,15 @@ class Generator(nn.Module):
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
@@ -93,12 +94,12 @@ class FashionGenerator(nn.Module):
         )
 
     def forward(self, x):
-        x = x.view(-1,100)
+        x = x.view(-1, 100)
         x = self.input(x)
         x = self.hidden1(x)
         x = self.hidden2(x)
         x = self.output(x)
-        return x.reshape(-1,1,28,28)
+        return x.reshape(-1, 1, 64, 64)
 
 
 class FashionDiscriminator(nn.Module):
@@ -128,10 +129,9 @@ class FashionDiscriminator(nn.Module):
         )
 
     def forward(self, x):
-        x = x.reshape(-1,28*28)
+        x = x.reshape(-1, 64 * 64)
         x = self.input(x)
         x = self.hidden1(x)
         x = self.hidden2(x)
         x = self.output(x)
         return x
-
